75121a33895eccd528d46f134b83b2c76ec7c10e
Dennis Francis <dennisfrancis.in@gmail.com>
2021-11-10 21:31:20 +0530

    apply clang-format on GMMCluster.hxx
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- GMMCluster.hxx
+++ GMMCluster.hxx
@@ -12,13 +12,9 @@
 #define NUMFUNCTIONS 1
 #define NUMARGS 4
 
-class GMMClusterImpl : public cppu::WeakImplHelper4
-<
-    com::github::dennisfrancis::XGMMCluster,
-    com::sun::star::sheet::XAddIn,
-    com::sun::star::lang::XServiceName,
-    com::sun::star::lang::XServiceInfo
->
+class GMMClusterImpl : public cppu::WeakImplHelper4<
+                           com::github::dennisfrancis::XGMMCluster, com::sun::star::sheet::XAddIn,
+                           com::sun::star::lang::XServiceName, com::sun::star::lang::XServiceInfo>
 {
 private:
     static const sal_Int32 nNumFunctions = NUMFUNCTIONS;
@@ -31,50 +27,48 @@ private:
     com::sun::star::lang::Locale aFuncLocale;
 
 public:
-
-    GMMClusterImpl()
-    {
-    }
+    GMMClusterImpl() {}
 
     // XGMMCluster
-    virtual ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Sequence< double > > SAL_CALL
-    gmmCluster(
-        const ::com::sun::star::uno::Sequence < ::com::sun::star::uno::Sequence < ::com::sun::star::uno::Any > >& data,
-        const ::com::sun::star::uno::Any& numClusters,
-        const ::com::sun::star::uno::Any& numEpochs,
-        const ::com::sun::star::uno::Any& numIterations);
+    virtual ::com::sun::star::uno::Sequence<::com::sun::star::uno::Sequence<double>>
+        SAL_CALL gmmCluster(const ::com::sun::star::uno::Sequence<
+                                ::com::sun::star::uno::Sequence<::com::sun::star::uno::Any>>& data,
+                            const ::com::sun::star::uno::Any& numClusters,
+                            const ::com::sun::star::uno::Any& numEpochs,
+                            const ::com::sun::star::uno::Any& numIterations);
 
     // XAddIn
-    rtl::OUString getProgrammaticFuntionName( const rtl::OUString& aDisplayName );
-    rtl::OUString getDisplayFunctionName( const rtl::OUString& aProgrammaticName );
-    rtl::OUString getFunctionDescription( const rtl::OUString& aProgrammaticName );
-    rtl::OUString getDisplayArgumentName( const rtl::OUString& aProgrammaticFunctionName, sal_Int32 nArgument );
-    rtl::OUString getArgumentDescription( const rtl::OUString& aProgrammaticFunctionName, sal_Int32 nArgument );
-    rtl::OUString getProgrammaticCategoryName( const rtl::OUString& aProgrammaticFunctionName );
-    rtl::OUString getDisplayCategoryName( const rtl::OUString& aProgrammaticFunctionName );
+    rtl::OUString getProgrammaticFuntionName(const rtl::OUString& aDisplayName);
+    rtl::OUString getDisplayFunctionName(const rtl::OUString& aProgrammaticName);
+    rtl::OUString getFunctionDescription(const rtl::OUString& aProgrammaticName);
+    rtl::OUString getDisplayArgumentName(const rtl::OUString& aProgrammaticFunctionName,
+                                         sal_Int32 nArgument);
+    rtl::OUString getArgumentDescription(const rtl::OUString& aProgrammaticFunctionName,
+                                         sal_Int32 nArgument);
+    rtl::OUString getProgrammaticCategoryName(const rtl::OUString& aProgrammaticFunctionName);
+    rtl::OUString getDisplayCategoryName(const rtl::OUString& aProgrammaticFunctionName);
 
     //  XLocalizable
-    void setLocale( const com::sun::star::lang::Locale& aLocale ) { aFuncLocale = aLocale; }
+    void setLocale(const com::sun::star::lang::Locale& aLocale) { aFuncLocale = aLocale; }
     com::sun::star::lang::Locale getLocale() { return aFuncLocale; }
 
     // XServiceInfo methods
     virtual ::rtl::OUString SAL_CALL getImplementationName();
-    virtual sal_Bool SAL_CALL supportsService( const ::rtl::OUString& aServiceName );
-    virtual ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames();
+    virtual sal_Bool SAL_CALL supportsService(const ::rtl::OUString& aServiceName);
+    virtual ::com::sun::star::uno::Sequence<::rtl::OUString> SAL_CALL getSupportedServiceNames();
 
     ::rtl::OUString getServiceName();
 
 private:
-
-    sal_Int32 getFunctionID( const ::rtl::OUString aProgrammaticFunctionName ) const;
+    sal_Int32 getFunctionID(const ::rtl::OUString aProgrammaticFunctionName) const;
 };
 
 ::rtl::OUString GMMClusterImpl_getImplementationName();
 
-sal_Bool SAL_CALL GMMClusterImpl_supportsService( const ::rtl::OUString& ServiceName );
+sal_Bool SAL_CALL GMMClusterImpl_supportsService(const ::rtl::OUString& ServiceName);
 
-::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL GMMClusterImpl_getSupportedServiceNames();
+::com::sun::star::uno::Sequence<::rtl::OUString> SAL_CALL GMMClusterImpl_getSupportedServiceNames();
 
-::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >
-SAL_CALL GMMClusterImpl_createInstance( const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > & rContext);
-
+::com::sun::star::uno::Reference<::com::sun::star::uno::XInterface>
+    SAL_CALL GMMClusterImpl_createInstance(
+        const ::com::sun::star::uno::Reference<::com::sun::star::uno::XComponentContext>& rContext);
