0772f000dd49de464503280846ed2ec4f7281e67
Dennis Francis <dennisfrancis.in@gmail.com>
2021-11-10 21:31:53 +0530

    apply clang-format on perf.hxx
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- perf.hxx
+++ perf.hxx
@@ -6,15 +6,15 @@
 class TimePerf
 {
 public:
-    TimePerf(const char *sMessage);
+    TimePerf(const char* sMessage);
     void Stop();
 
 private:
     std::chrono::high_resolution_clock::time_point nStart, nEnd;
-    const char *pStr;
+    const char* pStr;
 };
 
-TimePerf::TimePerf(const char *sMessage)
+TimePerf::TimePerf(const char* sMessage)
 {
     nStart = std::chrono::high_resolution_clock::now();
     pStr = sMessage;
@@ -23,6 +23,7 @@ TimePerf::TimePerf(const char *sMessage)
 void TimePerf::Stop()
 {
     nEnd = std::chrono::high_resolution_clock::now();
-    float fSec = float(std::chrono::duration_cast<std::chrono::milliseconds>(nEnd - nStart).count()) / 1000.0;
+    float fSec = float(std::chrono::duration_cast<std::chrono::milliseconds>(nEnd - nStart).count())
+                 / 1000.0;
     writeLog("DEBUG>>> Finished %s in %.4f seconds.\n", pStr, fSec);
 }
