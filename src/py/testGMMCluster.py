f68b793455c370d2ee51f0ca1dc961c2a197f894
Dennis Francis <dennisfrancis.in@gmail.com>
2021-12-02 22:35:41 +0530

    add quick test run of the addin

---
+++ src/py/testGMMCluster.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import sys
+import os
+import inspect
+import random
+
+sys.path.append('/usr/lib/libreoffice/program')
+
+cmd_folder = os.path.realpath(
+    os.path.abspath(
+        os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+import ClusterRows
+
+def main():
+    gmmImpl = ClusterRows.GMMClusterImpl()
+    data = tuple(((random.uniform(0,10), random.uniform(20, 30)) for i in range(100)))
+    ret = gmmImpl.gmmCluster(data, numClusters=3, numEpochs=20, numIterations=100)
+    print(ret)
+
+if __name__ == '__main__':
+    main()
