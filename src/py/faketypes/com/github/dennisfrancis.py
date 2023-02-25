d4c46f2f5c08d914a06d901a4cfcad7da55d3ee1
Dennis Francis <dennisfrancis.in@gmail.com>
2023-02-25 11:44:20 +0530

    fix type annotation of return value of gmmCluster
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- src/py/faketypes/com/github/dennisfrancis.py
+++ src/py/faketypes/com/github/dennisfrancis.py
@@ -3,5 +3,5 @@ from typing import Tuple
 class XDataCluster(object):
     def __init__(self):
         return
-    def gmmCluster(self, data: Tuple[Tuple[float]], numClusters, numEpochs, numIterations) -> Tuple[Tuple[float]]:
+    def gmmCluster(self, data: Tuple[Tuple[float]], numClusters, numEpochs, numIterations) -> Tuple[Tuple[float, ...]]:
         return ((-1, 0),)
