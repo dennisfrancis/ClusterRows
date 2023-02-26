f44ae0f7fde9757ca6a74998ea7c06a94493601e
Dennis Francis <dennisfrancis.in@gmail.com>
2023-02-26 15:21:46 +0530

    add faketypes in awt.py
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

---
+++ src/py/faketypes/com/sun/star/awt.py
@@ -0,0 +1,11 @@
+class XDialogEventHandler:
+    def __init__(self):
+        return
+
+class XTextListener:
+    def __init__(self):
+        return
+
+class XItemListener:
+    def __init__(self):
+        return
